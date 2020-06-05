113597999bda3d4233f6dabc76ee94afb4e04b86
Qccccc's laptop <896853205@qq.com>
2020-06-06 00:19:25 +0800

    perf: 删除部署时的source-map

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,11 @@
 const { override, fixBabelImports, addWebpackAlias } = require('customize-cra');
-const path = require('path');
+const { resolve } = require('path');
+
+const rewiredMap = config => {
+  config.devtool =
+    config.mode === 'development' ? 'cheap-module-source-map' : false; // 生产环境关闭sourcemap关闭
+  return config;
+};
 
 module.exports = override(
   fixBabelImports('import', {
@@ -8,8 +14,9 @@ module.exports = override(
     style: 'css',
   }),
   addWebpackAlias({
-    '@': path.resolve(__dirname, 'src'),
-    '@constants': path.resolve(__dirname, 'src/constants'),
-    '@components': path.resolve(__dirname, 'src/components'),
-  })
+    '@': resolve(__dirname, 'src'),
+    '@constants': resolve(__dirname, 'src/constants'),
+    '@components': resolve(__dirname, 'src/components'),
+  }),
+  rewiredMap
 );
